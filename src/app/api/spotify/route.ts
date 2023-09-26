30d655c4abad694138bde35e32901ac7ceaac7f9
DTA32 <97021335+DTA32@users.noreply.github.com>
2023-09-26 21:03:20 +0700

    spotify done (di local, gatau kalo di deploy hehe)

---
+++ src/app/api/spotify/route.ts
@@ -0,0 +1,45 @@
+import { NextResponse } from "next/server";
+import querystring from "querystring";
+
+const refreshToken = process.env.NEXT_PUBLIC_SPOTIFY_REFRESH_TOKEN;
+const basicAuth = process.env.NEXT_PUBLIC_SPOTIFY_BASIC_AUTH;
+const nowPlayingAPI = "https://api.spotify.com/v1/me/player/currently-playing";
+const tokenAPI = "https://accounts.spotify.com/api/token";
+
+const getAccessToken = async () => {
+  const response = await fetch(tokenAPI, {
+    method: "POST",
+    headers: {
+      Authorization: `Basic ${basicAuth}`,
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: querystring.stringify({
+      grant_type: "refresh_token",
+      refresh_token: refreshToken,
+    }),
+    next: { revalidate: 3600 },
+  }).then((res) => res.json());
+  return response;
+};
+
+const getNowPlaying = async () => {
+  const { access_token } = await getAccessToken();
+  const response = await fetch(nowPlayingAPI, {
+    headers: { Authorization: `Bearer ${access_token}` },
+    next: { revalidate: 300 },
+  });
+  if (response.status === 204) return NextResponse.json({ is_playing: false });
+  return response.json();
+};
+
+export async function GET() {
+  const response = await getNowPlaying();
+  if (response.error?.status) {
+    console.log(response.error);
+    return NextResponse.json({ error: "Error in fetching data" });
+  }
+  if (response.is_playing === false) {
+    return NextResponse.json({ is_playing: false });
+  }
+  return NextResponse.json(response);
+}
--- src/app/components/Spotify.tsx
+++ src/app/components/Spotify.tsx
@@ -1,26 +1,92 @@
+"use client";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 config.autoAddCss = false;
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
 import { faSpotify } from "@fortawesome/free-brands-svg-icons";
+import useSWR from "swr";
+import Image from "next/image";
+
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
+const serverURL =
+  process.env.NEXT_PUBLIC_NODE_ENV !== "production"
+    ? "http://localhost:3000"
+    : "https://dta32.github.io";
 
 export default function Spotify() {
+  const { data, error, isLoading } = useSWR(
+    `${serverURL}/api/spotify`,
+    fetcher
+  );
+  if (isLoading)
+    return (
+      <div className="hidden lg:block h-2/3 select-none w-[360px]">
+        <div className="flex flex-row px-2 h-full w-full items-center gap-x-2 animate-pulse">
+          <div className="bg-slate-700 h-10 w-10"></div>
+          <div className="grid grid-rows-2 h-full gap-y-2">
+            <div className="bg-slate-700 h-2 w-24 rounded self-end"></div>
+            <div className="bg-slate-700 h-4 w-48 rounded"></div>
+          </div>
+        </div>
+      </div>
+    );
+  if (error || !data)
+    return (
+      <div className="hidden lg:block h-2/3 select-none w-[360px]">
+        <div className="flex flex-row px-2 h-full w-full items-center gap-x-2 ">
+          <div>
+            <FontAwesomeIcon icon={faSpotify} size="2x" color="red" />
+          </div>
+          <div className="grid grid-rows-2 h-full">
+            <p className="text-gray-300 text-xs self-end">
+              Spotify listening activity
+            </p>
+            <p className="truncate w-full text-sm">Error fetching data</p>
+          </div>
+        </div>
+      </div>
+    );
+
+  let isPlaying = data.is_playing;
   return (
     <a
-      href="https://open.spotify.com/user/t56tgotgnyyhud9rkhvbabz7a"
-      className="hidden lg:block h-2/3 border border-slate-600 w-[360px]"
+      href={
+        isPlaying
+          ? data.item.external_urls.spotify
+          : "https://open.spotify.com/user/t56tgotgnyyhud9rkhvbabz7a"
+      }
+      className="hidden lg:block h-2/3 select-none w-[360px]"
     >
-      <div className="flex flex-row px-2 h-full w-full items-center gap-x-2">
-        <div>
-          <FontAwesomeIcon icon={faSpotify} size="2x" />
+      <div className="group relative h-full w-full">
+        <div className="flex flex-row px-2 h-full w-full items-center gap-x-2 group-hover:brightness-[0.25] duration-300">
+          <div>
+            {isPlaying ? (
+              <Image
+                src={data.item.album.images[0].url}
+                alt={`${data.item.name} album art`}
+                width={40}
+                height={40}
+              />
+            ) : (
+              <FontAwesomeIcon icon={faSpotify} size="2x" color="gray" />
+            )}
+          </div>
+          <div className="grid grid-rows-2 h-full">
+            <p className="text-gray-300 text-xs self-end">
+              {isPlaying
+                ? "Currently listening to"
+                : "click to view my profile :3"}
+            </p>
+            <p className="truncate w-full">
+              {isPlaying
+                ? `${data.item.artists[0].name} - ${data.item.name}`
+                : "Offline"}
+            </p>
+          </div>
         </div>
-        <div className="grid grid-rows-2 h-full">
-          <p className="text-gray-300 text-xs self-end">
-            Currently listening to
-          </p>
-          <p className="truncate w-full text-sm">
-            Tame Impala - The Less I Know The Better
+        <div className="flex opacity-0 group-hover:opacity-100 absolute inset-0 h-full w-full duration-300">
+          <p className="my-auto mx-auto text-center py-1 px-2 text-sky-300 after:content-['_↗']">
+            Open in Spotify
           </p>
         </div>
       </div>
